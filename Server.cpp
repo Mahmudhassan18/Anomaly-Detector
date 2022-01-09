1064baf97d9e2f5f3015e330f251baebe00b578e
Mahmud Hassan <mahmodhasan1001@gmail.com>
2022-01-09 17:22:20 +0200

    EX6 Server side

---
+++ Server.cpp
@@ -0,0 +1,69 @@
+#include "Server.h"
+
+string serverIO::read(){
+    char c = 0;
+    string s = "";
+    while(c!='\n'){
+        recv(clientID,&c,sizeof(char),0);
+        s += c;
+    }
+    return s;
+}
+
+void serverIO::write(string text){
+    const char* txt = text.c_str();
+    send(clientID,txt,strlen(txt),0);
+}
+
+void serverIO::write(float f){
+    ostringstream ss;
+    ss << f;
+    string s(ss.str());
+    write(s);
+}
+
+void serverIO::read(float* f){
+    //recv(clientID,f,sizeof(float),0);
+    // it will be already in the string line
+}
+
+Server::Server(int port)throw (const char*) {
+    flag = false;
+    fileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (fileDescriptor < 0)
+        throw "Socket failed";
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    if(::bind(fileDescriptor, (struct sockaddr*)&server, sizeof(server)) < 0)
+        throw "bind failed";
+    if (listen(fileDescriptor, 3) < 0)
+        throw "listen failed";
+
+};
+
+void Server::start(ClientHandler& ch)throw(const char*){
+    t = new thread([&ch, this]() {
+        while (!flag) {
+            socklen_t clientSize = sizeof(client);
+            int aClient = accept(fileDescriptor, (struct sockaddr *) &client, &clientSize);
+            if (aClient < 0)
+                throw "Accept failed";
+            ch.handle(aClient);
+            close(aClient);
+        }
+        close(fileDescriptor);
+    });
+}
+
+void Server::stop(){
+    flag = true;
+	t->join(); // do not delete this!
+}
+
+Server::~Server() {
+
+}
+
---
+++ Server.h
@@ -0,0 +1,71 @@
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <pthread.h>
+#include <thread>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include<signal.h>
+#include <sstream>
+
+#include "CLI.h"
+using namespace std;
+
+// edit your ClientHandler interface here:
+class ClientHandler{
+    public:
+    virtual void handle(int clientID)=0;
+};
+
+
+// you can add helper classes here and implement on the cpp file
+class serverIO:public DefaultIO{
+    int clientID;
+public:
+
+    serverIO(int clientID):clientID(clientID){}
+
+    virtual string read();
+    virtual void write(string text);
+
+    virtual void write(float f);
+
+    virtual void read(float* f);
+
+};
+
+
+// edit your AnomalyDetectionHandler class here
+class AnomalyDetectionHandler:public ClientHandler{
+	public:
+    virtual void handle(int clientID){
+        serverIO sio(clientID);
+        CLI cli(&sio);
+        cli.start();
+    }
+};
+
+
+// implement on Server.cpp
+class Server {
+	thread* t; // the thread to run the start() method in
+	int fileDescriptor;
+	sockaddr_in server;
+	sockaddr_in client;
+	bool flag;
+public:
+	Server(int port) throw (const char*);
+	virtual ~Server();
+	void start(ClientHandler& ch)throw(const char*);
+	void stop();
+};
+
+#endif /* SERVER_H_ */
